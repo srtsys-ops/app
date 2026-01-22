500239004feb8798d4cb05f1ea292ce1c604de21
srtsys-ops <srtsys@gmail.com>
2026-01-22 13:14:30 +0530

    Update multiple_disease_pred.py

--- multiple_disease_pred.py
+++ multiple_disease_pred.py
@@ -9,21 +9,7 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-st.markdown(
-    """
-    <style>
-    header[data-testid="stHeader"] {
-        display: none;
-    }
 
-    /* Remove extra top padding */
-    .block-container {
-        padding-top: 1rem;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 
 # Loading the saved models
@@ -514,6 +500,7 @@ if (selected == 'Parkinsons Prediction'):
                 st.success("🟢 No Parkinson’s Disease Detected")
 
     
+
 
 
 

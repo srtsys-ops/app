2bdd0b8883cc0ff85dafe1a8a5eea4064c910575
srtsys-ops <srtsys@gmail.com>
2026-01-28 11:51:04 +0530

    Update multiple_disease_pred.py

--- multiple_disease_pred.py
+++ multiple_disease_pred.py
@@ -153,7 +153,7 @@ with st.sidebar:
     selected = option_menu(
         "Select Prediction",
         ['Diabetes Prediction', 'Heart Disease Prediction', 'Parkinsons Prediction'],
-        icons=[🩸 , 'heart-pulse', 'person-lines-fill'],
+        icons=['🩸' , 'heart-pulse', 'person-lines-fill'],
         default_index=0,
         styles={
             "container": {"padding": "5px"},
@@ -737,6 +737,7 @@ if (selected == 'Parkinsons Prediction'):
                 st.success("🟢 No Parkinson’s Disease Detected")
 
     
+
 
 
 

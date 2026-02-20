04294e9f040bdbef3b8dd6c047cba7e130ff6445
srtsys-ops <srtsys@gmail.com>
2026-02-20 22:05:05 +0530

    Update multiple_disease_pred.py

--- multiple_disease_pred.py
+++ multiple_disease_pred.py
@@ -124,16 +124,27 @@ else:
 st.markdown(f"""
     <div class="fixed-header">
         <div class="fixed-header-inner">
-            <img src="data:image/png;base64,{logo_base64}">
-            <h1>PERIYAR UNIVERSITY</h1>
-            <div class="scrolling-text">
-                <h5>Name: THILAK S, 1st Year MSc Data Science. Project Coordinator€: Prof. Saravanan</h5>
+            
+            <!-- Left Logo -->
+            <img src="data:image/png;base64,{logo_base64}" class="header-logo-left">
+
+            <!-- Center Title Section -->
+            <div class="header-center">
+                <h1>PERIYAR UNIVERSITY</h1>
+                <div class="scrolling-text">
+                    <h5>
+                        Name: THILAK S, 1st Year MSc Data Science | 
+                        Project Coordinator: Prof. Saravanan
+                    </h5>
+                </div>
             </div>
-            <img src="data:image/png;base64,{periyar_base64}">
+
+            <!-- Right Image -->
+            <img src="data:image/png;base64,{periyar_base64}" class="header-logo-right">
+
         </div>
     </div>
-    """, unsafe_allow_html=True
- )
+""", unsafe_allow_html=True)
 
 #------------ Mmain Content Section  Start--------------------
 st.markdown('<div class="main-content">', unsafe_allow_html=True)
@@ -790,6 +801,7 @@ if (selected == 'Parkinsons Prediction'):
 
 #------------ Mmain Content Section End--------------------    
 st.markdown('</div>', unsafe_allow_html=True)
+
 
 
 
